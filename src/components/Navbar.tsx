509f1fd56744f56c5a6461aa904dab77742b1f29
Afreet Co <afreet.co20@gmail.com>
2021-11-11 00:02:23 +0530

    Added hover change in navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,36 +7,36 @@ export const Navbar = () => {
     <div className="w-full fixed z-50 bg-gradient-to-r from-black via-gray-900 to-gray-700">
       <nav className=" max-w-6xl mx-auto text-sm font-medium px-2 py-4 flex items-center justify-between">
         <NextLink href="/">
-          <a className="text-white text-sm font-bold leading-relaxed inline-block mr-4 md:mr-40 whitespace-nowrap uppercase">
+          <a className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block mr-4 md:mr-40 whitespace-nowrap uppercase">
             {app.name}
           </a>
         </NextLink>
 
         <div className="space-x-2">
           <NextLink href="/posts">
-            <a className="text-white text-sm font-bold leading-relaxed inline-block ">
+            <a className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block ">
               Posts
             </a>
           </NextLink>
           <NextLink href="/csr">
-            <a className="text-white text-sm font-bold leading-relaxed inline-block ">
+            <a className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block ">
               CSR
             </a>
           </NextLink>
           <NextLink href="/ssr">
-            <a className="text-white text-sm font-bold leading-relaxed inline-block ">
+            <a className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block ">
               SSR
             </a>
           </NextLink>
           <a
             href="/api/hello"
-            className="text-white text-sm font-bold leading-relaxed inline-block "
+            className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block "
           >
             Hello API
           </a>
           <a
             href="/api/posts"
-            className="text-white text-sm font-bold leading-relaxed inline-block "
+            className="text-white hover:text-gray-200 text-sm font-bold leading-relaxed inline-block "
           >
             Posts API
           </a>
